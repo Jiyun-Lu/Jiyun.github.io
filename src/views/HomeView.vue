b15c9163f7b01a55bab62f95c682bd870c73ed51
Jiyun <81668159+Jiyun-Lu@users.noreply.github.com>
2024-03-16 19:05:23 +0800

    Add files via upload

---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+ <div>
+ hello 你好
+ </div>
+</template>
+ 
+<script>
+export default {
+ name: '',
+props: {},
+ data () {
+ return {
+ }
+ },
+ components: {
+ },
+ methods: {
+ },
+ created () {
+ },
+ computed: {
+ }
+ }
+</script>
+ 
+<style scoped lang = "scss">
+ 
+</style>
